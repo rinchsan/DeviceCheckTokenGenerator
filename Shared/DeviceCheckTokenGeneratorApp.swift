92f893b08e8909624dc61097b419a27baf73d2f3
Masaya Hayashi <mcdonalds.only@gmail.com>
2021-02-27 18:47:10 +0900

    Initial Commit

---
+++ Shared/DeviceCheckTokenGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceCheckTokenGeneratorApp.swift
+//  Shared
+//
+//  Created by Masaya Hayashi on 2021/02/27.
+//
+
+import SwiftUI
+
+@main
+struct DeviceCheckTokenGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
